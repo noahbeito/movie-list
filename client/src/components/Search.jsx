0fb500d8675228691aa9278904abd56b3b585869
Noah Beito <noah.beito@gmail.com>
2023-04-12 12:35:12 -0700

    add handleClick functionn to search button and add handleChange function to text input

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Search = () => (
+// recieve handle change as a prop
+
+// set up an onChange for input, store this input somewhere
+
+// set up a handle click in go button that calls a
+// handle click function that changes state of movie list
+
+const Search = ( {handleChange, handleClick} ) => (
   <div>
-    <input type="text"></input>
-    <button>Go!</button>
+    <input type="text" onChange={(event) => (handleChange(event.target.value.toLowerCase()))}></input>
+    <button onClick={() => (handleClick())}>Go!</button>
   </div>
 );
 
