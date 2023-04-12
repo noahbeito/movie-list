758b1483a7f7f11c53c0aabe531c4039f80954db
Noah Beito <noah.beito@gmail.com>
2023-04-12 11:27:20 -0700

    added MovieList component and wrapped page title and new component in a div

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import movies from '../movieData.js';
+import MovieList from './MovieList.jsx';
 
 const App = (props) => (
-  <div>Hello World!</div>
+  <div>
+    <h1>Movie List</h1>
+    <MovieList movies={movies} />
+  </div>
+
 );
 
 export default App;
