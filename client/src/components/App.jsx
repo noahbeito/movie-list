16d5b19238d71de1b0c4300c467dd362b020032c
Noah Beito <noah.beito@gmail.com>
2023-04-12 11:43:25 -0700

    added state for movie list and set the default to our movies data. pass movie list state to our MovieList component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import movies from '../movieData.js';
 import MovieList from './MovieList.jsx';
+import Search from './Search.jsx';
 
-const App = (props) => (
-  <div>
-    <h1>Movie List</h1>
-    <MovieList movies={movies} />
-  </div>
+const { useState } = React;
 
-);
+const App = (props) => {
+  const [movieList, setMovieList] = useState(movies);
+
+  return (
+    <div>
+      <h1>Movie List</h1>
+      <Search />
+      <MovieList movies={movieList} />
+    </div>
+  );
+};
 
 export default App;
