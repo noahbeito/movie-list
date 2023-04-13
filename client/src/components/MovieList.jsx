47a6c868052f5868092ea6130dd0f335a6c99664
Noah Beito <noah.beito@gmail.com>
2023-04-12 17:24:06 -0700

    added prop handleWatched and passed it down to Movie component

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Movie from './Movie.jsx';
 
 
-const MovieList = ( {movies} ) => (
+const MovieList = ( {movies, handleWatched} ) => (
   // map over movies data and create
-  movies.map(movie => <Movie movie={movie} key={movie.title}/>)
+  movies.map(movie => <Movie movie={movie} handleWatched={handleWatched} key={movie.title}/>)
 );
 
 export default MovieList;
