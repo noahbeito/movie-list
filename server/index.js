e42c5c7ee25f37d8734d235ae7d83dc0578315f4
Noah Beito <noah.beito@gmail.com>
2023-04-22 15:44:01 -0700

    update server index file to use middleware, set the port, and properly route the requests

--- server/index.js
+++ server/index.js
@@ -2,8 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.set('port', 3000);
+
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+const router = require('./routes.js');
+
+app.use('/api', router);
+
+
 app.use(express.static('client/dist'));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 })
+
+module.exports.app = app;
